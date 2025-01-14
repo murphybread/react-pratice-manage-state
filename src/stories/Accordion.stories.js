2adc48752fb8916acd557a9f437a91510a74341d
Murphybread <nargene@gmail.com>
2025-01-15 01:02:01 +0900

    feat: add syncedinputs

---
+++ src/stories/Accordion.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Accordion from "../components/Accordion";
+
+export default {
+  title: "Button/Accordion",
+  component: Accordion,
+};
+
+const Template = () => <Accordion />;
+
+export const Default = Template.bind({});
+Default.storyName = "Default";
---
+++ src/stories/SyncedInputs.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SyncedInputs from "../components/SyncedInputs";
+
+export default {
+  title: "Input/SyncedInputs",
+  component: SyncedInputs,
+};
+
+const Template = () => <SyncedInputs />;
+
+export const Default = Template.bind({});
+Default.storyName = "Default";
