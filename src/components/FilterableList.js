b807d455988e0a4d427ead24239b71f22789b06f
Murphybread <nargene@gmail.com>
2025-01-15 01:22:17 +0900

    feat: add filterableList using data.js

---
+++ src/components/FilterableList.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { foods, filterItems } from "../data/data.js";
+
+export default function FilterableList() {
+  const [query, setQuery] = useState("");
+  const filteredItems = filterItems(foods, query);
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
+  return (
+    <>
+      <SearchBar query={query} onChange={handleChange} />
+      <hr />
+      <List items={filteredItems} />
+    </>
+  );
+}
+
+function SearchBar({ query, onChange }) {
+  return (
+    <label>
+      Search: <input value={query} onChange={onChange} />
+    </label>
+  );
+}
+
+function List({ items }) {
+  return (
+    <table>
+      {items.map((food) => (
+        <tr key={food.id}>
+          <td>{food.name}</td>
+          <td>{food.description}</td>
+        </tr>
+      ))}
+    </table>
+  );
+}
---
+++ src/stories/FilterableLis.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FilterableList from "../components/FilterableList";
+
+export default {
+  title: "Input/FilterableList",
+  component: FilterableList,
+};
+
+const Template = () => <FilterableList />;
+
+export const Default = Template.bind({});
+Default.storyName = "Default";
