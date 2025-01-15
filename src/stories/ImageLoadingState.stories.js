7ac1de1196451f609b7a3d32daa3b7e4d598b5de
Murphybread <nargene@gmail.com>
2025-01-15 21:14:15 +0900

    feat: add initialize state in imageloading

---
+++ src/stories/ImageLoadingState.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ImageLoadingState from "../components/ImageLoadingState";
+
+export default {
+  title: "Button/ImageLoadingState",
+  component: ImageLoadingState,
+};
+
+const Template = () => <ImageLoadingState />;
+
+export const Default = Template.bind({});
+Default.storyName = "Default";
